04b23f8ca732d70885c2abeaa20b9141a7d00637
chanatipbeam <chanatipthanomyat@gmail.com>
2024-08-08 11:38:55 +0700

    add column

--- NewLife_Web_api/Model/User.cs
+++ NewLife_Web_api/Model/User.cs
@@ -27,7 +27,53 @@ namespace NewLife_Web_api.Model
         [Column("role")]
         public string role { get; set; }
 
+        [Column("address")]
+        public string address { get; set; }
 
+        [Column("tel")]
+        public string tel{ get; set; }
+
+        [Column("gender")]
+        public string gender { get; set; }
+
+        [Column("age")]
+        public string age{ get; set; }
+
+        [Column("career")]
+        public string career{ get; set; }
+
+        [Column("num_of_fam_members")]
+        public string numOfFamMembers { get; set; }
+
+        [Column("experience")]
+        public string experience { get; set; }
+
+        [Column("size_of_residence")]
+        public string sizeOfResidence { get; set; }
+
+        [Column("type_of_residence")]
+        public string typeOfResidence { get; set; }
+
+        [Column("free_time_per_day")]
+        public string freeTimePerDay { get; set; }
+
+        [Column("reason_for_adoption")]
+        public string reasonForAdoption { get; set; }
+
+        [Column("interest_id_1")]
+        public string interestId1 { get; set; }
+
+        [Column("interest_id_2")]
+        public string interestId2 { get; set; }
+
+        [Column("interest_id_3")]
+        public string interestId3 { get; set; }
+
+        [Column("interest_id_4")]
+        public string interestId4 { get; set; }
+
+        [Column("interest_id_5")]
+        public string interestId5 { get; set; }
     }
 
 }
