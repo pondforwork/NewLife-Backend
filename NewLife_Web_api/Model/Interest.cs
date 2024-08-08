6f0c30f19286de65841536ddf67b0a64615dd147
chanatipbeam <chanatipthanomyat@gmail.com>
2024-08-08 14:05:22 +0700

    add interest

---
+++ NewLife_Web_api/Model/Interest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Interest
+{
+    [Key]
+    [Column("interest_id")]
+    public int interestlId { get; set; }
+
+    [Column("breed_id")]
+    public int breedId { get; set; }
+
+    [Column("size")]
+    public string size { get; set; }
+
+    [Column("sex")]
+    public string sex { get; set; }
+
+
+}
