3c7c1acd210690c44683f3771259a9b900aa65a9
pondforwork <pharadonsiriforwork@gmail.com>
2024-08-12 13:46:07 +0700

    patch delete notification adoption post

--- NewLife_Web_api/Controllers/NotificationAdoptionPost.cs
+++ NewLife_Web_api/Controllers/NotificationAdoptionPost.cs
@@ -39,9 +39,9 @@ namespace NewLife_Web_api.Controllers
 
             try
             {
-                var query = "INSERT INTO notification_adoption_post (notification_id, post_adoption_id, user_id, `description`, is_read, noti_date) " +
-                                           "VALUES (@p0, @p1, @p2, @p3, @p4, @p5)";
-                await _context.Database.ExecuteSqlRawAsync(query, newPost.notificationId, newPost.postAdoptionId, newPost.userId, newPost.description, newPost.isRead, newPost.notiDate);
+                var query = "INSERT INTO notification_adoption_post ( post_adoption_id, user_id, `description`, is_read, noti_date) " +
+                                           "VALUES (@p0, @p1, @p2, @p3, @p4)";
+                await _context.Database.ExecuteSqlRawAsync(query, newPost.postAdoptionId, newPost.userId, newPost.description, newPost.isRead, newPost.notiDate);
                 return Ok("Create Notification Success");
             }
             catch (Exception ex)
@@ -49,6 +49,71 @@ namespace NewLife_Web_api.Controllers
                 return BadRequest(new { message = "An error occurred while creating the post.", error = ex.Message });
             }
         }
+
+        [HttpPatch]
+        public async Task<IActionResult> Update([FromBody] NoficationAdoptionPost post)
+        {
+            if (post == null)
+            {
+                return BadRequest(new { message = "Post data is invalid." });
+            }
+            try
+            {
+                var notification = await _context.NoficationAdoptionPosts
+                 .FromSqlRaw("SELECT notification_id,post_adoption_id,user_id,`description`,is_read,noti_date FROM notification_adoption_post WHERE notification_id = @p0", post.notificationId)
+                 .FirstOrDefaultAsync();
+
+                if (notification == null)
+                {
+                    return NotFound();
+                }
+
+                var query = "UPDATE notification_adoption_post SET " +
+                            "user_id = @p1, " +
+                            "`description` = @p2, " +
+                            "is_read = @p3, " +
+                            "noti_date = @p4 " +
+                            "WHERE notification_id = @p0";
+                await _context.Database.ExecuteSqlRawAsync(query, post.postAdoptionId, post.userId, post.description, post.isRead, post.notiDate);
+                return Ok("Update Notification Success");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while updating the post.", error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest(new { message = "Invalid ID." });
+            }
+
+            try
+            {
+                var notification = await _context.NoficationAdoptionPosts
+                 .FromSqlRaw("SELECT notification_id,post_adoption_id,user_id,`description`,is_read,noti_date FROM notification_adoption_post WHERE notification_id = @p0", id)
+                 .FirstOrDefaultAsync();
+
+                if (notification == null)
+                {
+                    return NotFound();
+                }
+
+
+                var query = "DELETE FROM notification_adoption_post WHERE notification_id = @p0";
+                await _context.Database.ExecuteSqlRawAsync(query, id);
+                return Ok("Delete Notification Success");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while deleting the post.", error = ex.Message });
+            }
+        }
+
+
 
     }
 }
