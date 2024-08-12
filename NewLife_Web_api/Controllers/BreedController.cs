769bbdb3858d5b51adbc19e99057f371c5ba6f1f
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-08-12 18:11:31 +0700

    deleteId and post in breed

--- NewLife_Web_api/Controllers/BreedController.cs
+++ NewLife_Web_api/Controllers/BreedController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NewLife_Web_api.Model;
 
 namespace NewLife_Web_api.Controllers
 {
@@ -48,6 +49,53 @@ namespace NewLife_Web_api.Controllers
                 return StatusCode(500, $"An error occurred while retrieving the breeds: {ex.Message}");
             }
         }
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteBreed(int Id)
+        {
+            try
+            {
+                // Check if the breed exists before attempting to delete
+                var breeds = await _context.Breeds.FromSqlRaw("SELECT breed_id , animal_type, breed_name FROM breed WHERE breed_id = {0}", Id).FirstOrDefaultAsync();
+
+                if (breeds == null)
+                {
+                    return NotFound("breed not found.");
+                }
+
+                // Execute the delete command
+                await _context.Database.ExecuteSqlRawAsync("DELETE FROM breed WHERE breed_id = {0}", Id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex) here as needed
+                return StatusCode(500, $"An error occurred while deleting the interest: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Breed newPost)
+        {
+            if (newPost == null)
+            {
+                return BadRequest(new { message = "Post data is invalid." });
+            }
+
+            try
+            {
+                var query = "INSERT INTO breed ( breed_id, animal_type, breed_name) " +
+                                           "VALUES (@p0, @p1, @p2)";
+                await _context.Database.ExecuteSqlRawAsync(query, newPost.breedId, newPost.animalType, newPost.breedName);
+                return Ok("Create Notification Success");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while creating the breed.", error = ex.Message });
+            }
+        }
+
+
 
 
     }
