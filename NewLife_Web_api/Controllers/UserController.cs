d8e43ad5e4515d20b84fc9a1a703fe083f5ae9b4
Pharadon Sirijhan <64160142@go.buu.ac.th>
2024-08-16 19:51:16 +0700

    update user

--- NewLife_Web_api/Controllers/UserController.cs
+++ NewLife_Web_api/Controllers/UserController.cs
@@ -158,6 +158,98 @@ namespace NewLife_Web_api.Controllers
             return Ok(new { message = "User created successfully." });
         }
 
+        [HttpPatch("UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromForm] User user, IFormFile? image)
+        {
+            string imageFileName = "";
+
+            // Upload new image if provided
+            if (image != null)
+            {
+                try
+                {
+                    var result = await SaveImage(image);
+                    if (result is OkObjectResult okResult)
+                    {
+                        imageFileName = (string)((dynamic)okResult.Value).FileName;
+                    }
+                    else
+                    {
+                        return BadRequest(new { message = "Image upload failed." });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { message = "Image upload failed.", error = ex.Message });
+                }
+            }
+
+            var sqlQuery = @"
+                UPDATE user SET
+                    profile_pic = @profile_pic, 
+                    `name` = @name, 
+                    lastname = @lastname, 
+                    email = @email, 
+                    `password` = @password, 
+                    `role` = @role, 
+                    tel = @tel, 
+                    gender = @gender, 
+                    age = @age, 
+                    career = @career, 
+                    num_of_fam_members = @num_of_fam_members, 
+                    experience = @experience, 
+                    size_of_residence = @size_of_residence, 
+                    type_of_residence = @type_of_residence, 
+                    free_time_per_day = @free_time_per_day, 
+                    reason_for_adoption = @reason_for_adoption, 
+                    interest_id_1 = @interest_id_1, 
+                    interest_id_2 = @interest_id_2, 
+                    interest_id_3 = @interest_id_3, 
+                    interest_id_4 = @interest_id_4, 
+                    interest_id_5 = @interest_id_5, 
+                    address = @address
+                WHERE user_id = @user_id";
+
+                        var parameters = new[]
+                        {
+                            new MySqlParameter("@profile_pic", !string.IsNullOrEmpty(imageFileName) ? (object)imageFileName : DBNull.Value),
+                            new MySqlParameter("@name", user.name ?? (object)DBNull.Value),
+                            new MySqlParameter("@lastname", user.lastName ?? (object)DBNull.Value),
+                            new MySqlParameter("@email", user.email ?? (object)DBNull.Value),
+                            new MySqlParameter("@password", user.password ?? (object)DBNull.Value),
+                            new MySqlParameter("@role", user.role ?? (object)DBNull.Value),
+                            new MySqlParameter("@tel", user.tel ?? (object)DBNull.Value),
+                            new MySqlParameter("@gender", user.gender ?? (object)DBNull.Value),
+                            new MySqlParameter("@age", user.age),
+                            new MySqlParameter("@career", user.career ?? (object)DBNull.Value),
+                            new MySqlParameter("@num_of_fam_members", user.numOfFamMembers),
+                            new MySqlParameter("@experience", user.experience ?? (object)DBNull.Value),
+                            new MySqlParameter("@size_of_residence", user.sizeOfResidence ?? (object)DBNull.Value),
+                            new MySqlParameter("@type_of_residence", user.typeOfResidence ?? (object)DBNull.Value),
+                            new MySqlParameter("@free_time_per_day", user.freeTimePerDay),
+                            new MySqlParameter("@reason_for_adoption", user.reasonForAdoption ?? (object)DBNull.Value),
+                            new MySqlParameter("@interest_id_1", user.interestId1.HasValue ? (object)user.interestId1.Value : DBNull.Value),
+                            new MySqlParameter("@interest_id_2", user.interestId2.HasValue ? (object)user.interestId2.Value : DBNull.Value),
+                            new MySqlParameter("@interest_id_3", user.interestId3.HasValue ? (object)user.interestId3.Value : DBNull.Value),
+                            new MySqlParameter("@interest_id_4", user.interestId4.HasValue ? (object)user.interestId4.Value : DBNull.Value),
+                            new MySqlParameter("@interest_id_5", user.interestId5.HasValue ? (object)user.interestId5.Value : DBNull.Value),
+                            new MySqlParameter("@address", user.address ?? (object)DBNull.Value),
+                            new MySqlParameter("@user_id", user.userId)
+                        };
+
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync(sqlQuery, parameters);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "User update failed.", error = ex.Message });
+            }
+
+            return Ok(new { message = "User updated successfully." });
+        }
+
+
 
 
         [HttpDelete("{Id}")]
