315e35a1e3b37b7ab1cc8f53e29f77dce2faa1a5
Pharadon Sirijhan <pharadonsiriforwork@gmial.com>
2024-09-18 20:38:04 +0700

    add notification find owner post

---
+++ NewLife_Web_api/Controllers/NotificationFindOwnerPostController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewLife_Web_api.Model;
+
+namespace NewLife_Web_api.Controllers
+
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NofificationFindOwnerPostController : Controller
+    {
+
+    }
+}
