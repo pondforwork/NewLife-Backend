c71281215d9a54645090595b8e29debfef54129f
Pharadon Sirijhan <64160142@go.buu.ac.th>
2024-09-19 13:55:08 +0700

    delete missing post

--- NewLife_Web_api/Controllers/MissingPostController.cs
+++ NewLife_Web_api/Controllers/MissingPostController.cs
@@ -62,5 +62,83 @@ namespace NewLife_Web_api.Controllers
             }
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetData(int id)
+        {
+            try
+            {
+                var missingPost = await _context.MissingPosts.FromSqlRaw(@"
+                 SELECT 
+                        missing_post_id,
+                        user_id,
+                        image_1,
+                        image_2,
+                        image_3,
+                        image_4,
+                        image_5,
+                        image_6,
+                        image_7,
+                        image_8,
+                        image_9,
+                        image_10,
+                        `name`,
+                        breed_id,
+                        age,
+                        sex,
+                        is_need_attention,
+                        `description`,
+                        province_id,
+                        district_id,
+                        subdistrict_id,
+                        address_details,
+                        post_code,
+                        post_status,
+                        create_at,
+                        update_at,
+                        delete_at
+                    FROM 
+                        missing_post
+                    WHERE 
+                        missing_post_id = @p0
+                ", id).FirstOrDefaultAsync();
+
+                if (missingPost == null)
+                {
+                    return NotFound(new { message = "Missing Post not found." });
+                }
+
+                return Ok(missingPost);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while retrieving the Missing Post.", error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            try
+            {
+                var missingPost = await _context.MissingPosts.FindAsync(id);
+
+                if (missingPost == null)
+                {
+                    return NotFound(new { message = "Missing Post not found." });
+                }
+
+                _context.MissingPosts.Remove(missingPost);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Missing Post deleted successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while deleting the Missing Post.", error = ex.Message });
+            }
+        }
+
+
+
     }
 }
