c8b733ccf3fd165a159f9bc7e4c660d1093b4a14
Pharadon Sirijhan <pharadonsiriforwork@gmial.com>
2024-10-05 12:28:02 +0700

    getnewpet post

--- NewLife_Web_api/Controllers/AdoptionPostController.cs
+++ NewLife_Web_api/Controllers/AdoptionPostController.cs
@@ -77,6 +77,26 @@ namespace NewLife_Web_api.Controllers
             }
         }
 
+
+        [HttpGet("GetNewPet")]
+        public async Task<IActionResult> GetNewPet()
+        {
+            try
+            {
+                var query = "SELECT adoption_post_id, user_id, image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8, image_9, " +
+                    "image_10, name, breed_id, age, sex, is_need_attention, description, province_id, district_id, " +
+                    "subdistrict_id, address_details, adoption_status, post_status, create_at, update_at, delete_at " +
+                    "FROM adoption_post " +
+                    "ORDER BY create_at DESC";
+                List<AdoptionPost> posts = await _context.AdoptionPosts.FromSqlRaw(query).ToListAsync();
+                return Ok(posts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while retrieving the posts.", error = ex.Message });
+            }
+        }
+
         [HttpGet("GetPost/{id}")]
         public async Task<IActionResult> GetData(int id)
         {
